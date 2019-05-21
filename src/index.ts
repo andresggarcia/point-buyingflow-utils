1ba7baf06e9e0327d3d7f1e9f784dc5b779a97c7
Andrés García <andres.ggarcia@mercadolibre.com>
2019-05-21 16:04:45 -0300

    Fixed and bumped to v0.0.2

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import jwt from 'jsonwebtoken';
 
 export class Vault {
   public options: jwt.SignOptions;
-  private _secret: string;
+  private SECRET: string;
 
+  /**
+   * Instantiates the Vault class
+   * @param secret The seed string. 
+   * @param options Extra options for jwt
+   */
   constructor(secret: string, options: jwt.SignOptions = {
     expiresIn: '24h'
   }) {
@@ -11,26 +16,34 @@ export class Vault {
       throw new Error('Missing secret.');
     }
 
-    this._secret = secret;
+    this.SECRET = secret;
     this.options = options;
   }
 
+  /**
+   * Token signing
+   * @param {string} key
+   */
   private sign(key: string) {
     if (!key) {
       throw new Error('Missing key.');
     }
 
-    const token = jwt.sign({ key }, this._secret, this.options);
+    const token = jwt.sign({ key }, this.SECRET, this.options);
     return token;
   }
 
+
+  /**
+   * Token verification
+   * @param {string} token 
+   */
   private verify(token: string) {
-    jwt.verify(token, this._secret, (err, decoded) => {
+    jwt.verify(token, this.SECRET, (err, decoded) => {
       if (err) {
         throw new Error('Token is not valid or expired.');
-      } else {
-        return decoded;
       }
+      return decoded;
     });
   }
 }
